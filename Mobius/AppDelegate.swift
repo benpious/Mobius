793e4f120291598cef18bd59e17e53141524fe3c
benpious <benpious@gmail.com>
2016-09-18 15:50:10 -0700

    Initial Commit

---
+++ Mobius/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Mobius
+//
+//  Created by Benjamin Pious on 9/18/16.
+//  Copyright © 2016 benpious. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
